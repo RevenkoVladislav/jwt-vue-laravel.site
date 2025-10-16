5d5e32cd3efa700ecc0da6ee9c9c581d2dfe99de
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-16 23:57:53 +0600

    install JWT auth

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User as User;
 use App\Http\Controllers\Fuit as Fruit;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
 
 Route::prefix('user')->name('user.')->group(function () {
     Route::post('/', User\StoreController::class)->name('store');
